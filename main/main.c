0c485720724f21a1d572f60619e99e6dcfe2852e
Suren Poghosyan <suren.poghosian.18@gmail.com>
2025-07-01 23:22:00 +0400

    add a loop

--- main/main.c
+++ main/main.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 void app_main(void)
 {
-  printf("Start!\n");
   vTaskDelay(pdMS_TO_TICKS(2000));
-  printf("Delay Completed!\n");
+
+  for (int i = 1; i < 11; ++i)
+  {
+    printf("Start!\n");
+    vTaskDelay(pdMS_TO_TICKS(500));
+    printf("Delay Completed!\n");
+  }
 }
